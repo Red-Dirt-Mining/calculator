a131a0c5c0e6e17eff21d18633050de9ecb7487f
nonceraider <nonceraider@protonmail.com>
2022-09-04 20:35:22 -0500

    Refactor header component

--- src/components/basicBlurb.js
+++ src/components/basicBlurb.js
@@ -3,9 +3,23 @@ import { Heading, Text } from "@chakra-ui/react"
 export const BasicBlurb = () => {
   return (
     <div>
-      <Heading as={'h1'} size={'xl'} textAlign='center' color="white" sx={{ fontFamily: "Montserrat", fontWeight: 600 }}>THE OPEN SOURCE MINING CALCULATOR</Heading>
+      <Heading
+        as={'h1'}
+        textAlign='center'
+        color="white"
+        fontSize={{ base: 'xl', sm: '2xl', lg: '4xl' }}
+        fontWeight={600}
+        fontFamily={'Montserrat'}
+      >
+        THE OPEN SOURCE MINING CALCULATOR
+      </Heading>
       <br />
-      <Text textAlign='center' color="white" sx={{ fontFamily: "Montserrat", fontWeight: 500 }}>
+      <Text
+        textAlign='center'
+        color="white"
+        fontWeight={500}
+        fontFamily={'Montserrat'}
+      >
         Welcome to the Red Dirt Mining sats-denominated mining calculator.
       </Text>
       <br />
