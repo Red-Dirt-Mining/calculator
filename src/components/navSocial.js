f57319ea89f9bee43443e8a846f72828e0f3e3af
nonceraider <nonceraider@protonmail.com>
2022-09-10 16:23:11 -0500

    Floating social bar

---
+++ src/components/navSocial.js
@@ -0,0 +1,33 @@
+import { Box, Icon, VStack } from "@chakra-ui/react";
+import { MdOutlineMailOutline, MdRssFeed } from 'react-icons/md';
+import { FaTwitter, FaGithub } from 'react-icons/fa';
+
+export default function FloatingSocialNavbar () {
+  return (
+    <Box
+      position="fixed"
+      top={28}
+      right="0"
+      width={'32px'}
+      height={'140px'}
+      backgroundColor="#181919"
+      border="1px solid white"
+      p={2}
+    >
+      <VStack>
+        <a href={`https://twitter.com/reddirtmining`} target='_blank' rel='noopener noreferrer'>
+          <Icon as={FaTwitter} color="white" boxSize={5} />
+        </a>
+        <a href={`https://github.com/Red-Dirt-Mining/calculator`} target='_blank' rel='noopener noreferrer'>
+          <Icon as={FaGithub} color="white" boxSize={5} />
+        </a>
+        <a href={'mailto:info@reddirtmining.io'}>
+          <Icon as={MdOutlineMailOutline} color="white" boxSize={5} />
+        </a>
+        <a href={`https://anchor.fm/s/9b9b2abc/podcast/rss`} target='_blank' rel='noopener noreferrer'>
+          <Icon as={MdRssFeed} color="white" boxSize={5} />
+        </a>
+      </VStack>
+    </Box>
+  )
+}
